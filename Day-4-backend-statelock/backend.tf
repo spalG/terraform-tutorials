9c108b6f3748bca0e5fb8522247d4017977cc6da
Sagar Pal <sagar.pal35263@gmail.com>
2026-07-14 00:01:47 +0530

    Day 4

--- Day-4-backend-statelock/backend.tf
+++ Day-4-backend-statelock/backend.tf
@@ -3,7 +3,8 @@ terraform {
     bucket         = "my-terraform-state-bucket9811"
     key            = "network/ec2/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock"
+    #dynamodb_table = "terraform-state-lock"
+    use_lockfile   = true
     encrypt        = true
   }
 }
