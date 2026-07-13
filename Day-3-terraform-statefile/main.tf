df1c42630daf331125890e5c8704a3fb93ecb15b
Sagar Pal <sagar.pal35263@gmail.com>
2026-07-13 23:15:56 +0530

    Full AWS

--- Day-3-terraform-statefile/main.tf
+++ Day-3-terraform-statefile/main.tf
@@ -1,10 +1,110 @@
-resource "aws_instance" "web_server" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
-    Name        = var.instance_name
-    Environment = "learning"
-    ManagedBy   = "Terraform"
+    Name = "demo-vpc"
+  }
+}
+
+# -----------------------
+# Public Subnet
+# -----------------------
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# -----------------------
+# Internet Gateway
+# -----------------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "demo-igw"
+  }
+}
+
+# -----------------------
+# Route Table
+# -----------------------
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# -----------------------
+# Route Table Association
+# -----------------------
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# -----------------------
+# Security Group
+# -----------------------
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-security-group"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-security-group"
+  }
+}
+
+# -----------------------
+# EC2 Instance
+# -----------------------
+resource "aws_instance" "web" {
+  ami                    = "ami-002192a70217ac181" # Amazon Linux 2 (us-east-1)
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  associate_public_ip_address = true
+ # Replace with your AWS Key Pair name
+
+  tags = {
+    Name = "Terraform-EC2"
   }
 }
